910724f1b960e24b0f443370280795168bdf2826
= <eilersd23@gmail.com>
2023-09-29 09:26:30 -0400

    Fixes bug

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function createGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < size * size, i++;) {
+    for (let i = 0; i < size * size, i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
         gridSquare.addEventListener('mouseover', changeColor);
